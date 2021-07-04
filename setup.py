8a9f39d6f379e9e4f3470bf8029ca60a81be052d
Maximiliano Greco <6231413+asdf8601@users.noreply.github.com>
2021-07-04 19:10:44 +0200

    cln: fix flake8 violations
    bld: use pathlib instead of os.path

--- setup.py
+++ setup.py
@@ -1,34 +1,34 @@
-from setuptools import setup, find_packages
-from os import path
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
 import versioneer
 
-here = path.abspath(path.dirname(__file__))
+here = Path(__file__).expanduser().absolute().parent
+readme = here / "README.md"
+long_description = readme.read_text(encoding="utf-8")
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
-    name='quantkit',
+    name="quantkit",
     version=versioneer.get_version(),
-    description='Quant Kit',
+    description="Quantitative functions for finance analysis.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/mmngreco/quantkit',
-    author='mmngreco',
+    long_description_content_type="text/markdown",
+    url="https://github.com/mmngreco/quantkit",
+    author="mmngreco",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3 :: Only',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
     ],
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    python_requires='>=3.5, <4',
-    install_requires=['numpy', 'pandas'],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.5, <4",
+    install_requires=["numpy", "pandas"],
 )
